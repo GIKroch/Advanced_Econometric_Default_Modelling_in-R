fdb1188f58c1647d8f65926bdef455a3d175c057
GKroch <g.krochmal@student.uw.edu.pl>
2019-05-28 16:36:38 +0200

    final

--- some_stuff/workbook.R
+++ some_stuff/workbook.R
@@ -1,34 +1,33 @@
-setwd("C:/Users/user/Desktop/studia/Data Science/Semestr 2/Advanced_Econometrics/projekt/Advanced_Econometric_Default_Modelling_in-R")
-data <- read.csv2("imputedData.csv")
-missing_d <- data[rowSums(is.na(data))>0,]
-sum(missing_d$class)
-data <- data[rowSums(is.na(data))==0,]
-
-
-
-
-
-
-
 library(ggcorrplot)
 library(caret)
 library(logistf)
 library(dplyr)
-y_3 <- data$class
-data$class <- NULL
-X_3 <- data
+caret::findC
+install.packages("ResourceSelection")
+setwd("C:/Users/user/Desktop/studia/Data Science/Semestr 2/Advanced_Econometrics/projekt/Advanced_Econometric_Default_Modelling_in-R")
+data1 <- read.csv2("imputedData.csv")
+missing_d <- data1[rowSums(is.na(data1))>0,]
+sum(missing_d$class)
+data1 <- data1[rowSums(is.na(data1))==0,]
+coln <- as.character(read.csv2("column_names_1.csv", header = FALSE)$V2)
+coln <- c(coln, "BANKRUPTCY")
+colnames(data1) <- coln
+data1$INONE <- ifelse(NITA>0, 1, 0)
+data1$OENEG <- ifelse(TLTA>=1, 1, 0)
+y_3 <- data1$BANKRUPTCY
+data1$BANKRUPTCY <- NULL
+X_3 <- data1
 
-data1 <- data
 
 ## To see if we can limit number of features in our model first we decided to run Logit regression with one covariate at a time. 
 ## If the pvalue of the variable is > 0.25 we will drop it. 
 
 selected_names <- c()
 selected_names <- lapply(names(X_3),function(name){
-one_cov <- glm(y_3 ~ X_3[,name], family=binomial(link= "logit"))
+one_cov <- glm(y_3 ~ X_3[,name], family=binomial(link= "probit"))
 
 if (as.numeric(summary(one_cov)$coefficients[,4][2]) < 0.25){
-  print(summary(one_cov)$coefficients[,4][2])
+
   selected_names <- c(selected_names, name)
 }
 
@@ -36,23 +35,29 @@ if (as.numeric(summary(one_cov)$coefficients[,4][2]) < 0.25){
 
 selected_names <- unlist(selected_names)
 
+selected_names
 
+data1 <- data1 %>% dplyr::select(selected_names)
 
-## However after running the code it occured that with such an approach every variable seems to be significant
+## In this way we extracted 48 variables which pvalues for the regression y ~ variable[i] were less than 0,25
+## It helped us to reduce the dataset by 17 variables
+
 ## The another approach to limit number of variables is to drop those which are highly correlated. 
-## After the analysis of correlation data following columns were removed
+## After the analysis of correlation data we removed next 15 variables
 corr <- round(cor(X_3), 1)
 to_drop <- findCorrelation(corr, cutoff = 0.9, verbose = FALSE, names = F,
                 exact = ncol(corr) < 100)
 
 data1 <- data1[,-to_drop]
+data1$BANKRUPTCY <- y_3
 
 
-## Now we have 45 dependent variables. It is still a lot. Our next step will be to run stepwise regression in order to limit number of 
+
+## Now we have 33 dependent variables. It is still a lot. Our next step will be to run stepwise regression in order to limit number of 
 ## features
 library(MASS)
 # Fit the full model 
-full.model <- glm(formula = class ~.,data = data1, family=binomial(link= "logit"), control = list(maxit = 100))
+full.model <- glm(BANKRUPTCY ~.,data = data1, family=binomial(link= "probit"), control = list(maxit = 50))
 # Stepwise regression model
 step.model <- stepAIC(full.model, direction = "both", 
                       trace = FALSE)
@@ -62,6 +67,10 @@ step.model <- stepAIC(full.model, direction = "both",
 
 # After running the stepwise regression we ended up with the model which uses 27 dependent variables from which 21 are significant
 model_summary <- summary(step.model)
+model_summary
+
+
+lrtest(step.model, myprobit_final)
 
 ## Checking number of significant variables
 i <- 0
@@ -72,7 +81,14 @@ for (x in model_summary$coefficients[,4]){
 }
 
 
+myprobit2 <- glm(BANKRUPTCY~SIZE+TLTA+WCTA+OENEG+NITA+FUTL+SIZE2+CHSALES+CHSALES:INONE, 
+                 data=ohl_data, family=binomial(link="probit"))
+lrtest(myprobit2, myprobit_final)
 
+
+xd <- anova(myprobit_final, step.model, test= "LRT")
+
+as.data.frame(xd)
 # What should be stated here is that the standard logistic regression signalised us that there might be a problem of perfect or quasi perfect separation. 
 # It brigns the risk that obtained results might be inflated and do not properly reflect the reality. Surely there is a question
 # whether this is just some problem with the data we use or maybe it is the real thing for the whole population. If the second option
@@ -106,7 +122,15 @@ for (x in names(full_model_firths_summary$prob)){
 final.model_firths <- logistf(formula = class ~.,data = data1 %>% select(-insignificants_firths))
 
 final.model_firths$prob
+saveRDS(step.model, "step_model.RDS")
+xx <- readRDS("step_model.RDS")
 
+step.model$formula
+sum$coefficients
+mm <- broom::tidy(sum$coefficients)
+
+
+kableExtra::kable(mm)
 # Unfortunately after the analysis we had to drop the Logistic Regression with Firth's bias reduction. The stepwise regression in this case
 # is to computionaly demanding. Also, when we dropped insignificant variables we ended up with a model with 10 significant and 8 insignificant
 # variables we had to drop this way of solving our problem and just stay with the model we obtained before and it is the one we will use
